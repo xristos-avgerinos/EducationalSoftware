3c84841cc3d540acb0e6facce4d9616f309b3c41
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-05-24 17:00:30 +0300

    Add project files.

---
+++ EducationalSoftware/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationalSoftware.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult StudentsLogin()
+        {
+            return View();
+        }
+
+        public IActionResult DataScientistDirection()
+        {
+            return View();
+        }
+
+        public IActionResult CybersecuritySpecialistDirection()
+        {
+            return View();
+        }
+
+        public IActionResult SoftwareEngineerDirection()
+        {
+            return View();
+        }
+
+        public IActionResult UxDesignerDirection()
+        {
+            return View();
+        }
+
+        public IActionResult AiSpecialistDirection()
+        {
+            return View();
+        }
+
+        public IActionResult WebDeveloperDirection()
+        {
+            return View();
+        }
+    }
+}
